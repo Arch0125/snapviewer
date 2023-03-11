81557c068895c0dcf8388fb201528299f42ba9ea
Archisman Das <72722967+Arch0125@users.noreply.github.com>
2023-03-11 05:47:20 +0530

    add scw deposit feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,63 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { MetaMaskInpageProvider } from '@metamask/providers'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { ChainId } from "@biconomy/core-types";
+import SmartAccount from "@biconomy/smart-account";
+import { useAccount, useSigner } from "wagmi";
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
+declare global {
+  interface Window{
+    ethereum?:MetaMaskInpageProvider
+  }
+}
+
 export default function Home() {
+
+  const defaultSnapOrigin = `local:http://localhost:8080`;
+
+  const connectSnap = async (
+    snapId: string = defaultSnapOrigin,
+    params: Record<"version" | string, unknown> = {}
+  ) => {
+    await window.ethereum?.request({
+      method: "wallet_requestSnaps",
+      params: {
+        [snapId]: params,
+      },
+    });
+  };
+
+  const callSnap = async()=>{
+    await window.ethereum?.request({
+      method:"wallet_invokeSnap",
+      params:{
+        snapId : defaultSnapOrigin,
+        request:{
+          method:"init_aa",
+        },
+      },
+    });
+  }
+
+  const deposit = async()=>{
+    await window.ethereum?.request({
+      method:"wallet_invokeSnap",
+      params:{
+        snapId : defaultSnapOrigin,
+        request:{
+          method:"deposit_aa",
+        },
+      },
+    });
+  }
+
   return (
     <>
       <Head>
@@ -14,110 +67,29 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+      <div className='flex flex-col w-screen h-screen items-center justify-center bg-black' >
+        <p className='text-white text-5xl font-bold'>aa-snap37</p>
+        <p className='text-white text-2xl font-thin'>Redefining wallet experience - Native AA support for MetaMask</p>
+        <br/>
+        <br/>
+        <p className='text-white text-2xl mb-2 font-thin'>Integrate in your Dapp</p>
+        <div className='flex flex-col px-4 py-2 bg-gray-600 items-center justify-center rounded-xl' >
+          <p className='text-white text-1xl '>npm i @aa-snap37</p>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <br/>
+        <div className='flex flex-row' >
+          <Link href='https://github.com/Arch0125/snap4337'><p className='text-white text-2xl p-3 font-thin'>GitHub</p></Link>
+          <Link href='https://www.npmjs.com/package/aa-snap37'><p className='text-white text-2xl p-3 font-thin'>npm</p></Link>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <br/>
+        <br/>
+        <p className='text-white text-3xl mb-2 font-bold'>Try it out now! without connecting MetaMask to the Site</p>
+        <div className='flex flex-row' >
+          <button className='bg-blue-600 font-semibold text-white px-4 py-2 m-4 rounded-xl' onClick={()=>connectSnap()}>Install Snap</button>
+          <button className='bg-blue-600 font-semibold text-white px-4 py-2 m-4 rounded-xl' onClick={()=>callSnap()}>Connect SCW</button>
+          <button className='bg-blue-600 font-semibold text-white px-4 py-2 m-4 rounded-xl' onClick={()=>deposit()}>Deposit Funds</button>
         </div>
-      </main>
+      </div>
     </>
   )
 }
